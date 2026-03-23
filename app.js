1642ccaacc3a44c667eaaac21056c86e8c1f7c72
Your Name <patrickbrogan@Patricks-MacBook-Pro.local>
2026-03-23 13:12:09 -0400

    tested working version

--- app.js
+++ app.js
@@ -1,3 +1,20 @@
-const readline = require('readline-sync');
-let name = readline.question("What's your name? ");
-console.log("Hello " + name);
+const input = require('readline-sync');
+
+// Ask name
+let name = input.question("What is your name? ");
+console.log("Hey " + name + "!");
+
+// Ask 5 questions
+let q1 = input.question("What is 2 + 2? ");
+let q2 = input.question("Type the word hello: ");
+let q3 = input.questionInt("Enter a number: ");
+let q4 = input.question("What color do you like? ");
+let q5 = input.question("type yes: ");
+
+// Print answers
+console.log("\nYour answers:");
+console.log(q1);
+console.log(q2);
+console.log(q3);
+console.log(q4);
+console.log(q5);
